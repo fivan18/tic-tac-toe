5c6a36a70a52a02b3f7f848114e39cda362892c6
Ivan <fivanunam@gmail.com>
2020-01-09 17:56:28 -0600

    basic algorithm for tic tac toc interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,16 @@
 #!/usr/bin/env ruby
 
-puts "Hello world!"
+# get started, show welcome message.
+
+# get the name of each player
+
+# chose the name aleatory who's will go first
+
+while # current player not win
+    # switch player
+    # display dashboar
+    # ask the player to chose a place in the dashboard
+end
+
+# display the winner
+
