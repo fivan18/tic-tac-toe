009f11ce16f51e9a462836dff4d52d647cf94b66
Ivan <fivanunam@gmail.com>
2020-01-10 16:24:32 -0600

    fix arr instance variable in Dashboard class

--- lib/dashboard.rb
+++ lib/dashboard.rb
@@ -3,7 +3,7 @@ class Dashboard
     attr_reader :arr
 
     def initialize
-        @arr = %w[ none 1 2 3 4 < 6 < 8 9 ]
+        @arr = %w[ none 1 2 3 4 5 6 7 8 9 ]
     end
 
     def is_there_a_place?
