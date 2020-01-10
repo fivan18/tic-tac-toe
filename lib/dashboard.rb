14fab280fbd84ddb21a1c7f1ba7eb1a636647f68
Ivan <fivanunam@gmail.com>
2020-01-10 15:53:48 -0600

    add is_there_a_place? instance method to the class Dashboard

--- lib/dashboard.rb
+++ lib/dashboard.rb
@@ -1,6 +1,15 @@
 class Dashboard
+    LINES_TO_WIN = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
     attr_reader :arr
+
     def initialize
         @arr = %w[ none 1 2 3 4 5 6 7 8 9 ]
+    end
+
+    def is_there_a_place?
+        self.arr.each do |item|
+            return true if item != 'none' && item != '✘' && item != '●'
+        end
+        false
     end
 end
