7ff6715b85c98bbf0a9c1b43a84ac0b4eb846f17
Ivan <fivanunam@gmail.com>
2020-01-10 16:18:26 -0600

    add is_there_a_winner? instance method to Dashboard class

--- lib/dashboard.rb
+++ lib/dashboard.rb
@@ -3,12 +3,20 @@ class Dashboard
     attr_reader :arr
 
     def initialize
-        @arr = %w[ none 1 2 3 4 5 6 7 8 9 ]
+        @arr = %w[ none 1 2 3 4 < 6 < 8 9 ]
     end
 
     def is_there_a_place?
         self.arr.each do |item|
             return true if item != 'none' && item != '✘' && item != '●'
+        end
+        false
+    end
+
+    def is_there_a_winner?
+        LINES_TO_WIN.each do |line|
+            return true if self.arr[line[0]] == self.arr[line[1]] && 
+                self.arr[line[1]] == self.arr[line[2]]
         end
         false
     end
