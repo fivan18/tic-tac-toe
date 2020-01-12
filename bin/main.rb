4c71c849a9d8c62e37107ac4d86c89c0c89c7c44
Ivan <fivanunam@gmail.com>
2020-01-12 12:08:56 -0600

    run rubocop on main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,89 +1,81 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-require "tty-prompt"
-require_relative "../lib/player"
-require_relative "../lib/dashboard"
+require 'tty-prompt'
+require_relative '../lib/player'
+require_relative '../lib/dashboard'
 
 def print_dashboard(arr)
-    prompt = TTY::Prompt.new
+  prompt = TTY::Prompt.new
 
-    justify = ' ' * 7
-    separation = justify + '-------------'
+  justify = ' ' * 7
+  separation = justify + '-------------'
+  prompt.ok(separation)
+
+  i = 0
+  while i <= 6
+    prompt.ok(justify + "| #{arr[1 + i]} | #{arr[2 + i]} | #{arr[3 + i]} |")
     prompt.ok(separation)
+    i += 3
+  end
+end
 
-    i = 0
-    while i <= 6
-        prompt.ok(justify + "| #{arr[1 + i]} | #{arr[2 + i]} | #{arr[3 + i]} |")
-        prompt.ok(separation)
-        i = i + 3
-    end
-end 
+def arr_players
+  prompt = TTY::Prompt.new
 
-def get_players
-    prompt = TTY::Prompt.new
-
-    players = Array.new(2)
-    while players[0].nil? || players[0].alias_player == players[1].alias_player
-        players[0] = Player.new(prompt.ask("Alias for player 1?") { |q| q.required true }, '✘')
-        players[1] = Player.new(prompt.ask("Alias for player 2?") { |q| q.required true }, '●')
-    end
-    players.shuffle!
+  players = Array.new(2)
+  while players[0].nil? || players[0].alias_player == players[1].alias_player
+    players[0] = Player.new(prompt.ask('Alias for player 1?') { |q| q.required true }, '✘')
+    players[1] = Player.new(prompt.ask('Alias for player 2?') { |q| q.required true }, '●')
+  end
+  players.shuffle!
 end
 
 def play_game(players)
-    prompt = TTY::Prompt.new
+  prompt = TTY::Prompt.new
 
-    current_player = 0
-    dashboard = Dashboard.new
-    while dashboard.is_there_a_place? && !dashboard.is_there_a_winner?      
-        print_dashboard(dashboard.arr)
-    
-        place = prompt.ask(
-            "#{players[current_player].alias_player}," + 
-            " chose a place (1-9)? #{players[current_player].symbol}") do |q|
-                q.in '1-9'
-                q.messages[:range?] = 'Try again please...'
-            end
-        
-        current_player = current_player == 0 ? 1 : 0 if 
-            dashboard.add_move(place, players[current_player].symbol)
-    end
-    current_player = current_player == 0 ? 1 : 0
-
-    display_result(current_player, players, dashboard) 
-end
-
-def display_result(current_player, players, dashboard) 
-    prompt = prompt = TTY::Prompt.new
-
+  current_player = 0
+  dashboard = Dashboard.new
+  while dashboard.is_there_a_place? && !dashboard.is_there_a_winner?
     print_dashboard(dashboard.arr)
 
-    if dashboard.is_there_a_winner?
-        prompt.ok("\n\n#{players[current_player].alias_player} wins!!!!!")
-    else
-        prompt.ok("It is a tie...")
+    place = prompt.ask(
+      "#{players[current_player].alias_player}," \
+      " chose a place (1-9)? #{players[current_player].symbol}"
+    ) do |q|
+      q.in '1-9'
+      q.messages[:range?] = 'Try again please...'
     end
+
+    current_player = current_player.zero? ? 1 : 0 if
+      dashboard.add_move(place, players[current_player].symbol)
+  end
+  current_player = current_player.zero? ? 1 : 0
+
+  display_result(current_player, players, dashboard)
+end
+
+def display_result(current_player, players, dashboard)
+  prompt = TTY::Prompt.new
+
+  print_dashboard(dashboard.arr)
+
+  if dashboard.is_there_a_winner?
+    prompt.ok("\n\n#{players[current_player].alias_player} wins!!!!!")
+  else
+    prompt.ok('It is a tie...')
+  end
 end
 
 prompt = TTY::Prompt.new
 prompt.ok('Get start with Tic Tac Toe')
-print_dashboard(%w[ none ✘ ✘ ● ✘ ● ✘ ● ✘ ● ])
-prompt.keypress("Press enter to continue...", keys: [:return])
+print_dashboard(%w[none ✘ ✘ ● ✘ ● ✘ ● ✘ ●])
+prompt.keypress('Press enter to continue...', keys: [:return])
 
-players = get_players()
+players = arr_players
 
-loop do 
-    play_game(players)   
-    
-    break unless prompt.yes?("Do you like to play again?")
+loop do
+  play_game(players)
+
+  break unless prompt.yes?('Do you like to play again?')
 end
-
-
-
-
-
-
-
-
-
-
