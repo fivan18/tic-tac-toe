8480be661e0a02888734132a113f5cebf924708e
Ivan <fivanunam@gmail.com>
2020-01-11 23:17:01 -0600

    fix indentation spaces

--- bin/main.rb
+++ bin/main.rb
@@ -3,51 +3,51 @@
 require "tty-prompt"
 
 def print_dashboard(arr)
-    prompt = TTY::Prompt.new
+  prompt = TTY::Prompt.new
 
-    justify = ' ' * 7
-    separation = justify + '-------------'
+  justify = ' ' * 7
+  separation = justify + '-------------'
+  prompt.ok(separation)
+
+  i = 0
+  while i <= 6
+    prompt.ok(justify + "| #{arr[1 + i]} | #{arr[2 + i]} | #{arr[3 + i]} |")
     prompt.ok(separation)
-
-    i = 0
-    while i <= 6
-        prompt.ok(justify + "| #{arr[1 + i]} | #{arr[2 + i]} | #{arr[3 + i]} |")
-        prompt.ok(separation)
-        i = i + 3
-    end
+    i = i + 3
+  end
 end 
 
 def get_players_name
-    prompt = TTY::Prompt.new
+  prompt = TTY::Prompt.new
 
-    players = Array.new(2, '')
-    while players[0] == players[1]
-        players[0] = prompt.ask("Alias for player 1?") do |q|
-            q.required true
-        end
-        players[1] = prompt.ask("Alias for player 2?") do |q|
-            q.required true
-        end
+  players = Array.new(2, '')
+  while players[0] == players[1]
+    players[0] = prompt.ask("Alias for player 1?") do |q|
+      q.required true
     end
-    players.shuffle!
+    players[1] = prompt.ask("Alias for player 2?") do |q|
+      q.required true
+    end
+  end
+  players.shuffle!
 end
 
 def play_game(current_player, players_name, dashboard)
-    prompt = TTY::Prompt.new
+  prompt = TTY::Prompt.new
 
-    3.times do |_num|              #this will for a while
-        current_player = current_player == players_name[0] ? players_name[1] : players_name[0]
-        
-        print_dashboard(dashboard)
+  3.times do |_num|              #this will for a while
+    current_player = current_player == players_name[0] ? players_name[1] : players_name[0]
     
-        place = prompt.ask("#{current_player}, chose a place (1-9)?") do |q|
-            q.in '1-9'
-            q.messages[:range?] = 'Try again please...'
-        end
-        dashboard[place.to_i] = '✘'
-    end
+    print_dashboard(dashboard)
 
-    prompt.ok("\n\n#{current_player} wins!!!!!")
+    place = prompt.ask("#{current_player}, chose a place (1-9)?") do |q|
+      q.in '1-9'
+      q.messages[:range?] = 'Try again please...'
+    end
+    dashboard[place.to_i] = '✘'
+  end
+
+  prompt.ok("\n\n#{current_player} wins!!!!!")
 end
 
 prompt = TTY::Prompt.new
@@ -58,11 +58,11 @@ prompt.keypress("Press enter to continue...", keys: [:return])
 players_name = get_players_name()
 
 loop do 
-    current_player = players_name[0]
-    dashboard = %w[ none 1 2 3 4 5 6 7 8 9 ]
-    play_game(current_player, players_name, dashboard)   
-    
-    break unless prompt.yes?("Do you like to play again?")
+  current_player = players_name[0]
+  dashboard = %w[ none 1 2 3 4 5 6 7 8 9 ]
+  play_game(current_player, players_name, dashboard)   
+  
+  break unless prompt.yes?("Do you like to play again?")
 end
 
 
