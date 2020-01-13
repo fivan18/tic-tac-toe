cb2930ac78b0bd624c1ae4e5bd9ac73a816a27fa
Ivan <fivanunam@gmail.com>
2020-01-13 14:06:33 -0600

    add test cases for there_a_winner? method

--- spec/dashboard_spec.rb
+++ spec/dashboard_spec.rb
@@ -65,14 +65,62 @@ RSpec.describe Dashboard do
       expect(dashboard.there_a_place?).to eql(false)
     end    
   end
-end
 
-=begin
-RSpec.describe Dashboard do
-  describe '' do
-    it '' do
-      expect().to eql()
+  describe '#there_a_winner?' do
+    it 'wins becouse of line 1 2 3' do
+      fill_places(%w[1 2 3], dashboard, '✘')
+      expect(dashboard.there_a_winner?).to eql(true)
+    end
+
+    it 'wins becouse of line 4 5 6' do
+      fill_places(%w[4 5 6], dashboard, '✘')
+      expect(dashboard.there_a_winner?).to eql(true)
+    end
+
+    it 'wins becouse of line 7 8 9' do
+      fill_places(%w[7 8 9], dashboard, '✘')
+      expect(dashboard.there_a_winner?).to eql(true)
+    end
+
+    it 'wins becouse of line 1 4 7' do
+      fill_places(%w[1 4 7], dashboard, '✘')
+      expect(dashboard.there_a_winner?).to eql(true)
+    end
+
+    it 'wins becouse of line 2 5 8' do
+      fill_places(%w[2 5 8], dashboard, '✘')
+      expect(dashboard.there_a_winner?).to eql(true)
+    end
+
+    it 'wins becouse of line 3 6 9' do
+      fill_places(%w[3 6 9], dashboard, '✘')
+      expect(dashboard.there_a_winner?).to eql(true)
+    end
+
+    it 'wins becouse of line 1 5 9' do
+      fill_places(%w[1 5 9], dashboard, '✘')
+      expect(dashboard.there_a_winner?).to eql(true)
+    end
+
+    it 'wins becouse of line 3 5 7' do
+      fill_places(%w[3 5 7], dashboard, '✘')
+      expect(dashboard.there_a_winner?).to eql(true)
+    end
+
+    it 'both are winners' do
+      fill_places(%w[1 2 3], dashboard, '●')
+      fill_places(%w[4 5 6], dashboard, '✘')
+      expect(dashboard.there_a_winner?).to eql(true)
+    end
+
+    it 'anyone has done a move' do
+      expect(dashboard.there_a_winner?).to eql(false)
+    end
+
+    it 'there is no winner' do
+      fill_places(%w[1 4], dashboard, '●')
+      fill_places(%w[2 9], dashboard, '✘')
+      expect(dashboard.there_a_winner?).to eql(false)
     end
   end
 end
-=end
